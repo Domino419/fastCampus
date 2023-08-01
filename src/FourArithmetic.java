0ada680672f4c90b3b711b1704ba21cb73d43352
Domino419 <noris419@naver.com>
2023-08-01 23:19:30 +0900

    진도율 7.8% [총 57h]

---
+++ src/FourArithmetic.java
@@ -0,0 +1,12 @@
+public class FourArithmetic {
+    public static void main(String[] args) {
+        //두개의 정수는 12와 2 이고 변수명은 A, B
+        int a =12 ;
+        int b = 2 ;
+        System.out.println( "a+b = " + (a+b) );
+        System.out.println( "a-b = " + (a-b) );
+        System.out.println( "a*b = " + a*b );
+        System.out.println( "a/b = " + a/b );
+
+    }
+}
