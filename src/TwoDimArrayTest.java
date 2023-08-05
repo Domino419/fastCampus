8f4c5faf686523aec0b0a30a211d1e5ad9cd8c4f
Domino419 <noris419@naver.com>
2023-08-06 00:29:15 +0900

    진도율 32.4% [총 57h]

--- src/TwoDimArrayTest.java
+++ src/TwoDimArrayTest.java
@@ -4,6 +4,7 @@ public class TwoDimArrayTest {
         // int [햏][열] = new int [행][열]
         int[][] aa = new int[2][4];
 
+        // 출력을 위한 샘플 데이터 생성~
         aa = new int[][]{
                 {10, 11, 12, 13},
                 {20, 21, 22, 23},
@@ -19,7 +20,7 @@ public class TwoDimArrayTest {
                 } else {   // 한줄을 다 채우면 1부터 다시 시작
                     System.out.println();
                     System.out.print(aa[i][j]+ " ");
-                    count = 1 ;
+                    count = 1 ;  // count 초기화~
                 }
             }
         }
